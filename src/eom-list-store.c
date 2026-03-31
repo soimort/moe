395d04131762782b8858797547d4a5f2b3d2c6e3
Mort Yao <soi@mort.ninja>
2026-03-31 23:27:58 +0200

    revert fd5015a (store: Add any image file in the current directory to the store)

--- src/eom-list-store.c
+++ src/eom-list-store.c
@@ -471,8 +471,7 @@ directory_visit (GFile *directory,
 	name = g_file_info_get_name (children_info);
 
 	if (!g_str_has_prefix (name, ".")) {
-		/* We support opening any image type, so let eom to add any images in the current directory to the store */
-		if (g_content_type_is_mime_type (mime_type, "image/*") || eom_image_is_supported_mime_type (mime_type)) {
+		if (eom_image_is_supported_mime_type (mime_type)) {
 			load_uri = TRUE;
 		}
 	}
